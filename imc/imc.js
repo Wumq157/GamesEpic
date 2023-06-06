0bdd46397db850c89cf11445bfb0cb79d2cd690f
Otávio/Wumq <128698772+Wumq157@users.noreply.github.com>
2023-06-06 11:49:42 -0300

    .

--- imc/imc.js
+++ imc/imc.js
@@ -1,3 +1,7 @@
 var peso = 95
 var altura = 1.80
-ver altura2 = 
+var altura2 = altura ** 2
+var imc = peso % altura
+
+document.write(`Seu imc é de ${imc}, com base nesse valor você deve procurar a ajuda correta.`)
+
