1b5b054ec73a4210ea57fd751140626bf7b7fc09
Otávio/Wumq <128698772+Wumq157@users.noreply.github.com>
2023-06-06 20:09:19 -0300

    Update imc.js

--- imc/imc.js
+++ imc/imc.js
@@ -1,7 +1,7 @@
 var peso = Number(window.prompt("Diga qual o seu peso:"))
 var altura = Number(window.prompt("Dga qual a sua Altura:"))
 var altura2 = altura ** 2
-var imc = peso / altura
+var imc = peso / altura2
 
 document.write(`Seu imc é de ${imc}, com base nesse valor você deve procurar a ajuda correta.`)
 
